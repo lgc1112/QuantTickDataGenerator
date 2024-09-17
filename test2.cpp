7e06490cab852f420de03f63565fd2d678d3b856
regangcli <regangcli@tencent.com>
2024-09-17 20:22:23 +0800

    finish

---
+++ test2.cpp
@@ -0,0 +1,66 @@
+#include <system_error>
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+
+#include "logger.h"
+#include "transaction_mgr.h"
+#include "live_tarding_data_simulator.h"
+#include "snapshot_checker.h"
+
+int main()
+{
+    // 获取当前时间
+    std::time_t beginTime = std::time(0);
+    std::tm *beginLocalTime = std::localtime(&beginTime);
+
+    // 打印当前时间，格式为小时:分钟:秒
+    std::cout << "BeginTime: " << std::setw(2) << std::setfill('0') << beginLocalTime->tm_hour << ":" << std::setw(2)
+              << std::setfill('0') << beginLocalTime->tm_min << ":" << std::setw(2) << std::setfill('0')
+              << beginLocalTime->tm_sec << std::endl;
+
+    // 初始化交易管理器
+    TransactionMgr transMgr;
+    transMgr.SetPreClosePrice(168.998);
+    
+    // 初始化交易数据模拟器，并注册order transaction事件处理函数
+    LiveTradingDataSimulator liveTradingDataSimulator;
+    liveTradingDataSimulator.RegisterEventHandler(
+        DataEventType::RECV_ORDER,
+        std::function<void(void *)>([&transMgr](void *data) { transMgr.OnReceiveOrder(data); }));
+    liveTradingDataSimulator.RegisterEventHandler(
+        DataEventType::RECV_TRANSACTION,
+        std::function<void(void *)>([&transMgr](void *data) { transMgr.OnReceiveTransaction(data); }));
+
+    // 初始化实盘数据模拟器
+    int ret = liveTradingDataSimulator.Init();
+    if (ret)
+    {
+        LOG_ERROR1("Init liveTradingDataSimulator error!");
+        return ret;
+    }
+
+    // 运行实盘数据模拟器
+    liveTradingDataSimulator.Run();
+
+    // 导出生成tick数据
+    transMgr.DumpSnapshotToFile("data/GenTick.csv");
+
+    // 获取结束时间
+    std::time_t endTime = std::time(0);
+    std::tm *endLocalTime = std::localtime(&endTime);
+
+    // 检查tick数据是否正确率
+    TickDataChecker::CheckTickDataExistPercent("data/Tick.csv", transMgr.GetSnapshot());
+
+    // 打印结束时间，格式为小时:分钟:秒
+    std::cout << "EndTime: " << std::setw(2) << std::setfill('0') << endLocalTime->tm_hour << ":" << std::setw(2)
+              << std::setfill('0') << endLocalTime->tm_min << ":" << std::setw(2) << std::setfill('0')
+              << endLocalTime->tm_sec << std::endl;
+
+
+    std::cout << "DiffTime: " << endTime - beginTime << "s" << std::endl;
+
+    return 0;
+}
